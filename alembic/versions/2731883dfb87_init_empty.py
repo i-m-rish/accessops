fee85e49e9037f55e493d0a4b546ce0d63d9e3c9
Rish <rsrishsingh@gmail.com>
2026-01-05 05:07:21 +0530

    DB: initial Alembic migration and Postgres config

---
+++ alembic/versions/2731883dfb87_init_empty.py
@@ -0,0 +1,26 @@
+"""init (empty)
+
+Revision ID: 2731883dfb87
+Revises: 
+Create Date: 2026-01-05 04:54:37.634464
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '2731883dfb87'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
